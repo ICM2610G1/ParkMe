1db84dca3c6976a6f8fcacca4437b4cd10892500
sophieMjs <sophie.mejia.sc@gmail.com>
2026-04-20 15:02:47 -0500

    fixed ux

--- app/src/main/java/com/example/parkme/screens/LogIn.kt
+++ app/src/main/java/com/example/parkme/screens/LogIn.kt
@@ -150,6 +150,7 @@ fun LogIn(navController: NavController, viewModel: AppViewModel) {
             }
         }
 
+
         Spacer(modifier = Modifier.height(16.dp))
 
         Text("¿No tienes cuenta?")
