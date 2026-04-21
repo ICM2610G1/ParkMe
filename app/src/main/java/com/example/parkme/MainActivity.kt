f2375600c8835a1360eb60d297a87d2fe1dc4c20
nezsnt <santiagoibanez2580@gmail.com>
2026-04-21 01:20:26 -0500

    Acelerometro (Accidente)

--- app/src/main/java/com/example/parkme/MainActivity.kt
+++ app/src/main/java/com/example/parkme/MainActivity.kt
@@ -1,26 +1,60 @@
 package com.example.parkme
 
+import android.content.Context
+import android.content.Intent
 import android.hardware.Sensor
 import android.hardware.SensorManager
 import android.location.Geocoder
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.fragment.app.FragmentActivity
 import com.example.parkme.navigation.Navigation
+import android.net.Uri
+import androidx.activity.ComponentActivity
+import com.example.parkme.utils.ShakeDetector
 
 lateinit var sensorManager: SensorManager
 var lightSensor: Sensor? = null
 lateinit var geocoder: Geocoder
-class MainActivity : FragmentActivity() {
+var accelerometer: Sensor? = null
+lateinit var shakeDetector: ShakeDetector
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         geocoder = Geocoder(this)
-        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
         lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
-        enableEdgeToEdge()
+
+        shakeDetector = ShakeDetector {
+            simulateEmergencyCall()
+        }
+
         setContent {
             Navigation()
         }
+    }
+
+
+    private fun simulateEmergencyCall() {
+        val intent = Intent(Intent.ACTION_DIAL).apply {
+            data = Uri.parse("tel:123")
+        }
+        startActivity(intent)
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        accelerometer?.let {
+            sensorManager.registerListener(shakeDetector, it, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(shakeDetector)
     }
 }
