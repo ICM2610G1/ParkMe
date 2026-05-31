8ed93816e39cb87ed8d2d9e01e5511c9fd3c8bce
Esteban Blanco <esteban.blanco@javeriana.edu.co>
2026-05-31 15:03:13 -0500

    media funcion de cambio de reglas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.compose.ui.text)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.foundation)
     implementation(libs.androidx.lifecycle.runtime.ktx)
